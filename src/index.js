9392908573edc745efab671927b69007690015b3
twk2442 <twk2442@gmail.com>
2021-06-07 21:58:54 +0900

    3

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import App from './App'; //   뒤에 app은 파일 이름 app.js
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <App />  
   </React.StrictMode>,
   document.getElementById('root')
 );
