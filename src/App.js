a1c7c37f6e092d874fe9668ba893ac3b29fe4b6c
twk2442 <twk2442@gmail.com>
2021-06-10 22:42:22 +0900

    react-class :기초개념, 컴포넌트,props

--- src/App.js
+++ src/App.js
@@ -1,7 +1,37 @@
 import "./App.css";
 
+function Subject(props) {
+  return (
+    <header>
+      <h1>{props.title}</h1>
+      {props.sub}
+    </header>
+  );
+}
+
+function Toc() {
+  return (
+    <nav>
+      <ul>
+        <li>HTML</li>
+        <li>CSS</li>
+        <li>JavaScript</li>
+      </ul>
+    </nav>
+  );
+}
+function Content(props) {
+  return <article>{props.cont}</article>;
+}
+
 function App() {
-  return <div className="App">hello, react!</div>;
+  return (
+    <div className="App">
+      <Subject title="WEB" sub="world wide web" />
+      <Toc />
+      <Content cont="HyperText Markup Language, HTML" />
+    </div>
+  );
 } //  리엑트는 가장 바깥쪽에는 하나의 태그로 이루어진다.
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App'; //   뒤에 app은 파일 이름 app.js
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App"; //   뒤에 app은 파일 이름 app.js
+import reportWebVitals from "./reportWebVitals";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />  
-  </React.StrictMode>,
-  document.getElementById('root')
+    <App />
+  </React.StrictMode>, // 여기서 <App /> jsx문법이자 리액트 엘리먼트
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
